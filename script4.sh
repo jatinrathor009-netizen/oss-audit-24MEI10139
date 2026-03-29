5a0ae5f27b70355d4dd51a043b2439a9957e54f6
Jatin Rathor <jatinrathor009@gmail.com>
2026-03-29 22:57:08 +0530

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ============================================================
 # Script 4: Log File Analyzer
-# Author: Ravi Kumar Pal  | Reg No: 24MIP10162
+# Author: Jatin Rathor  | Reg No: 24MEI10139
 # Course: Open Source Software | Chosen Software: Python
 # Purpose: Reads a log file, counts keyword occurrences,
 #          and prints a summary with matching lines
